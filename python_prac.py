1227e0c752d9a08a8290fd216c872e0438d1929c
DK_Admin <dk_admin@Macs-MBP.home>
2020-06-17 15:36:18 -0700

    my first python practice code

---
+++ python_prac.py
@@ -0,0 +1,27 @@
+a = [1,2,3,4,5]
+print(a)
+
+oddnum=[1,3,5,7,9]
+evennum=[2,4,6,8,10]
+allnum=oddnum + evennum
+print(allnum)
+
+number=1*3/3
+print(number)
+
+remainder=11%3
+print(remainder)
+
+squared= 7**2
+print(squared)
+
+cubed= 2**3
+print(cubed)
+
+lotsofhello="hello  "*10
+print(lotsofhello)
+
+helloworld="hello" + " " +"world" 
+print(helloworld)
+
+print([1,2,3]*3)
